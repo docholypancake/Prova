1589b05b7f5c60449ff470184797bed2db199e5a
Oleh Blazhko <blazhkooleh@gmail.com>
2026-06-01 23:51:41 +0300

    posthog on client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,16 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext'
 
+import posthog from 'posthog-js'
+
+
 const queryClient = new QueryClient()
+
+posthog.init('phc_CWfQiwKxgZ5JZy3NEMX3Wy33Tng7s9Uvzo44yMfaxjqk', {
+  api_host: 'https://eu.i.posthog.com',
+  person_profiles: 'identified_only',
+})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
